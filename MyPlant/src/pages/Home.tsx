7bdc6d7dae1a3809211ad10157415f4804844183
evgenu <evgenyyyiii@gmail.com>
2020-05-21 18:43:45 +0300

    [Update] Styling and filled home page

--- MyPlant/src/pages/Home.tsx
+++ MyPlant/src/pages/Home.tsx
@@ -1,8 +1,29 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { 
+  IonButtons, 
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonCard,
+  IonImg,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonButton,
+  IonIcon
+} from '@ionic/react';
+import { shareSocialOutline, heartOutline } from 'ionicons/icons';
 import React from 'react';
+import { useParams } from 'react-router';
 import './Home.css';
 
 const Page: React.FC = () => {
+
+  const { name } = useParams<{ name: string; }>();
 
   return (
     <IonPage>
@@ -21,6 +42,25 @@ const Page: React.FC = () => {
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+
+        <IonCard>
+          <IonImg src={'https://techcrunch.com/wp-content/uploads/2019/08/20190530_Beginner_Plants_0007.jpg?w=730&crop=1'}></IonImg>
+          <IonCardHeader>
+            <IonCardTitle>AI Farm</IonCardTitle>
+            <IonCardSubtitle>The farm you need at home</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>
+              AI Farm is a project, which cares for your plants automaticaly.
+            </p>
+            <IonItem>
+              <IonButton fill="solid">Learn More</IonButton>
+              <IonIcon icon={heartOutline} slot='end' />
+              <IonIcon icon={shareSocialOutline} slot='end' />
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
--- MyPlant/src/pages/Profile.tsx
+++ MyPlant/src/pages/Profile.tsx
@@ -1,6 +1,26 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonAvatar } from '@ionic/react';
+import { 
+  IonButtons, 
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonAvatar, 
+  IonList, 
+  IonItem,
+  IonNote
+} from '@ionic/react';
 import React from 'react';
 import './Profile.css';
+
+
+const personal_data = {
+  name: 'William Hodge',
+  age: '39',
+  plants: '9',
+  favourite_plant: 'Lotus'
+}
 
 const Profile: React.FC = () => {
   
@@ -27,6 +47,19 @@ const Profile: React.FC = () => {
           <img src='https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' alt=''/>
         </IonAvatar>
         <p id='name'>William Hodge</p>
+
+        <IonList>
+          <IonItem>
+          <IonNote>Age: {personal_data.age}</IonNote>
+          </IonItem>
+          <IonItem>
+          <IonNote>Plants: {personal_data.plants}</IonNote>
+          </IonItem>
+          <IonItem>
+          <IonNote>Favourite Plant: {personal_data.favourite_plant}</IonNote>
+          </IonItem>
+        </IonList>
+
       </IonContent>
     </IonPage>
   );
